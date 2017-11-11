873f5b8d11939e58f4f38d0de793a3dd9eadb138
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-11 17:27:45 +0000

    rental car project

---
+++ rentalcarscript.js
@@ -0,0 +1,29 @@
+var cars = {
+name: "Enterprise Car Rental", 
+types: ['economy', 'midsize'],
+economy: 80,
+econBooked: 0,
+midsize: 150,
+midBooked: 0,
+}
+
+window.onload = function() {
+    document.getElementById("busName").innerHTML = cars.name;
+}
+
+function econAvail() {
+    cars.econBooked++;
+    document.getElementById("econAvail").innerHTML = cars.economy - cars.econBooked;
+}
+
+function midAvail() {
+    cars.midBooked++;
+    document.getElementById("midAvail").innerHTML = cars.midsize - cars.midBooked;
+}
+
+function getCars() {
+  document.getElementById("econCars").innerHTML  = cars.types[0];
+  document.getElementById("midCars").innerHTML = cars.types[1];
+  document.getElementById("econAvail").innerHTML = cars.economy - cars.econBooked;
+  document.getElementById("midAvail").innerHTML = cars.midsize - cars.midBooked;
+}
