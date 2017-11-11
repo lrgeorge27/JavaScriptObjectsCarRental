fa75558a0895081077fe03357400f5c57ce0cc74
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-11 18:56:21 +0000

    moved onclick to js file

--- rentalcarscript.js
+++ rentalcarscript.js
@@ -1,31 +1,31 @@
 var cars = {
-name: "Enterprise Car Rental", 
-types: ['economy', 'midsize'],
-economy: 80,
-//econBooked: 0,
-midsize: 150,
-//midBooked: 0,
-}
+    name: "Enterprise Car Rental", 
+    types: ['economy', 'midsize'],
+    economy: 80,
+    //econBooked: 0,
+    midsize: 150,
+    //midBooked: 0,
+    }
 
 window.onload = function() {
     document.getElementById("busName").innerHTML = cars.name;
-}
+    }
 
-function econAvail() {
+document.getElementById("econRent").onclick = function() {
     //cars.econBooked++;
     cars.economy--;
     document.getElementById("econAvail").innerHTML = cars.economy; //- cars.econBooked;
 }
 
-function midAvail() {
+document.getElementById("midRent").onclick = function() {
     //cars.midBooked++;
     cars.midsize--;
     document.getElementById("midAvail").innerHTML = cars.midsize; // - cars.midBooked;
 }
 
-function getCars() {
+document.getElementById("update").onclick = function() {
   document.getElementById("econCars").innerHTML  = cars.types[0];
   document.getElementById("midCars").innerHTML = cars.types[1];
   document.getElementById("econAvail").innerHTML = cars.economy; // - cars.econBooked;
-  document.getElementById("midAvail").innerHTML = cars.midsize //- cars.midBooked;
+  document.getElementById("midAvail").innerHTML = cars.midsize; //- cars.midBooked;
 }
