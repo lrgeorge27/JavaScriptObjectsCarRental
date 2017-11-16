d3bf324a10c1619d9161d31d24f0b6dd83da6824
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-16 18:15:16 +0000

    object still doesn't work

--- script3.js
+++ script3.js
@@ -10,35 +10,28 @@ var cars = {
     priceMid: "$100",
     priceLux: "$120"
 };
-
 var Rental = function(name, selection) {
     this.name = name,
         this.car = selection;
 };
-
 var renter = new Rental(document.getElementById("name"), document.getElementById("carSelection"));
-
 window.onload = function() {
     document.getElementById("busName").innerHTML = cars.name;
 };
 
 function getCars() {
     var car = document.getElementById("carSelection").value;
-
     switch (car) {
-
         case "economy":
             cars.economy--;
             document.getElementById("econAvail").innerHTML = "Economy:" + cars.economy;
             document.getElementById("econ").innerHTML = "Economy:" + cars.priceEcon;
             break;
-
         case "midsize":
             cars.midsize--;
             document.getElementById("midAvail").innerHTML = "Midsize:" + cars.midsize;
             document.getElementById("mid").innerHTML = "Midsize:" + cars.priceMid;
             break;
-
         case "luxury":
             cars.luxury--;
             document.getElementById("luxAvail").innerHTML = "Luxury:" + cars.luxury;
@@ -46,22 +39,14 @@ function getCars() {
             break;
     }
 }
-
 // function addRenter(renterName, selection) {
-//     // var selection = document.getElementsByTagId("carSelection");
-//     // var renterName = document.getElementById("name");
+//     var selection = document.getElementsByTagId("carSelection");
+//     var renterName = document.getElementById("name");
 //     var rental = document.getElementsByTagName("form");
-//     var text = "";
-//     var i;
-//     for (i = 0; i < rental.length; i++) {
-//         text += rental.elements[i].value + "<br>";
-
 //         rental.push({
 //             name: renterName,
 //             car: selection
 //         });
 //     }
 //}
-
-
 console.log(new Rental());
